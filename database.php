509c42fca8c864fbc7a864ced73b3aaf84e48f6d
mukk88 <markwoo@gmail.com>
2013-08-11 11:45:57 -0600

    adding php to check databse

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+// Create connection
+$con=mysqli_connect("mysql.markwoo.i-xo.net","mukk88","dbPa$$w0rd","markwoo");
+
+// Check connection
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  else{
+  	echo "connected"
+  }
+?>
