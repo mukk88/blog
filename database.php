737bd82c3ac90c34cfb33face15537cb2b034cdd
mukk88 <markwoo@gmail.com>
2013-08-11 23:55:37 -0600

    getting usernames

--- database.php
+++ database.php
@@ -6,9 +6,19 @@ try{
                     'mukk88',
                     'dbPa$$w0rd',
                     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    die(json_encode(array('outcome' => true)));
+
+    foreach($db->query('SELECT * FROM users') as $row) {
+    	echo $row['username'].;
+	}
+
+
+    // die(json_encode(array('outcome' => true)));
 }
+
+
 catch(PDOException $ex){
-    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+    die(json_encode(array('outcome' => false, 'message' => 'error')));
 }
+
+
 ?>
