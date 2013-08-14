4b1254b472f8cfd9956176ef972dcc429a8eed6e
mukk88 <markwoo@gmail.com>
2013-08-13 18:06:43 -0600

    add id and main

--- quickstart.php
+++ quickstart.php
@@ -139,9 +139,10 @@
                       foreach ($result as $row) {
                         echo "<li>";
                         echo "<img src='images/loveny.jpg' class='thumb' />";
-                        echo "<h4><span class='name'>".$row['title']."</span>";
+                        echo "<h4><span id = '".$row['id']."' class='name'>".$row['title']."</span>";
                         echo "<span class='category'></span>".$row['hash']."</h4>";
                         echo "<p class='description'>".$row['description']."</p>";
+                        echo "<p class='main'>".$row['words']."</p>";
                         echo "</li>";
                       }
 
