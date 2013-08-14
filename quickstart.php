98f6da3cc432cbd7cacb07d76ea424063b432048
mukk88 <markwoo@gmail.com>
2013-08-13 18:14:21 -0600

    reverting to verify

--- quickstart.php
+++ quickstart.php
@@ -137,12 +137,11 @@
                               'mukk88','dbPa$$w0rd', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
                       $result = $dbh->query("SELECT * FROM posts");
                       foreach ($result as $row) {
-                        $id = $row['id'];
                         echo "<li>";
                         echo "<img src='images/loveny.jpg' class='thumb' />";
+                        echo "<h4><span class='name'>".$row['title']."</span>";
                         echo "<span class='category'></span>".$row['hash']."</h4>";
                         echo "<p class='description'>".$row['description']."</p>";
-                        echo "<p class='main'>".$row['words']."</p>";
                         echo "</li>";
                       }
 
